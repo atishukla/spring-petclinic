352876e1d956ba4819f59dbe64ae74d7aec6c63d
atishukla <atishayshukla@gmail.com>
2020-12-24 14:15:22 +0100

    Added jenkins pipeline script

---
+++ pipelines/pipeline.groovy
@@ -0,0 +1,47 @@
+pipeline {
+  agent {
+    kubernetes {
+      defaultContainer 'jnlp'
+      yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+    - name: 'maven'
+      image: maven:3.3.9
+      command:
+        - cat
+      tty: true
+    - name: 'dind'
+      image: docker:dind
+      securityContext:
+        privileged: true
+    - name: 'kubectl'
+      image: tiborv/aws-kubectl
+      command:
+        - cat
+      tty: true
+"""
+    }
+  }
+
+  environment {
+    VERSION = ""
+  }
+
+  stages {
+    stage('Checkout') {
+      steps {
+        script {
+          deleteDir()
+          checkout scm
+          def matcher = readFile('pom.xml') =~ '<version>(.+?)</version>'
+          def current_version = matcher ? matcher[0][1] : '0.1.0'
+          VERSION = current_version+'.'+BUILD_NUMBER
+        }
+      }
+    }
+  }
+  
+}
+
